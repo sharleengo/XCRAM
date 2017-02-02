271913c7678ad930cc0982ab556c98862953b62c
savanah18 <gpagluba@up.edu.ph>
2017-02-02 20:40:16 +0800

    Update __init__.py

--- 05 Implementation/taskOverflow/Data/__init__.py
+++ 05 Implementation/taskOverflow/Data/__init__.py
@@ -28,7 +28,7 @@ of the Department of Computer Science, College of Engineering,
 University of the Philippines, Diliman for the AY 2016-2017
 
 Gerry Agluba Jr.
-last updated on February 2,2017
+last updated on January 31,2017
 Initial Software for Data Classes , its structures and methods.
 
 File created on January 29,2017.
@@ -44,32 +44,52 @@ from __future__ import print_function
 import heapq
 import random
 
+
+'''
+class Task is a superclass. This describes the primary structure of a task.
+Attrubutes involve title and duration.
+'''
 class Task():
 	def __init__(self, title,duration):
-		self.title=title
-		self.duration=duration
+		self.title=title #this variable describes the title of title task
+		self.duration=duration 	#this variable describes the duration of the task
 
 
+'''
+class FixTask is a subclass of Task. FixTask is a task that cannot be moved.
+Attributes involve title,duration,mustart and mustend
+'''
 class FixTask(Task):
 	def __init__(self,title,duration,mustart,mustend):
-		Task.__init__(self,title,duration)
-		self.mustart=mustart
-		self.mustend=mustend
+		Task.__init__(self,title,duration) 	
+		self.mustart=mustart 	#this variable indicates where the FixTask must start
+		self.mustend=mustend 	#this variable indicates where the FixTask must end
 
+
+
+'''
+Class FlexibleTask is a subclass of Task. FlexibleTask is a task that can be deleted and partitioned.
+Attributes involve title,priority,duration,lowerbound,and upperbound
+'''
 class FlexibleTask(Task):
 	def __init__(self,title,duration,priority,lowerbound,upperbound):
 		Task.__init__(self,title,duration)
-		self.priority=priority
-		self.lowerbound=lowerbound
-		self.upperbound=upperbound
+		self.priority=priority 	#this variable indicates the priority of a flexible Task, lower value means higher priority
+		self.lowerbound=lowerbound #this variable indicates the upper allowance when a flexible task must at least start
+		self.upperbound=upperbound #this variable indicates the lower allowance when a flexible task must at most end
 
 
+'''
+Class TimeBlock is a block of time where tasks are allocated.
+Attributes involve startTime,endTime,span and status.
+Methods are isFree and isEnoudh
+'''
 class TimeBlock():
 	def __init__(self,startTime,endTime,span,status):
-		self.startTime=startTime
-		self.endTime=endTime
-		self.span=span
-		self.status=status
+		self.startTime=startTime #this variable describes TimeBlock's initial time
+		self.endTime=endTime #this variable describes TimeBlock's end time
+		self.span=span #this variable describes the timespan or the space of the block
+		self.status=status #this variable describes if the timeBlock is allocated to a fix task, flexible task or None.
 
 
 	'''	
@@ -103,15 +123,21 @@ class TimeBlock():
 			return False
 
 	
-
+'''
+Class AllocationSpace is the backbound embodying all the timeblocks,tasks and priorityQueues involved in Alloation.
+Primary attribute is name.
+Methods are getData,AllocateTime,AllocateTimeFix,SearchStartingTimeBlock,SearchStartingFlexibleTaskTimeBlock,
+inTheMiddle,LeftSided,RightSided,exactlyFitted,spltAndReturnMiddle,splitAndReturnLedt,splitAndReturnRight,
+AllocateMaxTime,LocateKick,LocateKickFix,Kick and Merge
+'''
 class AllocationSpace():
 
 	def __init__(self,name):
 		'''this constructor initializes the class and sets the name; new space is created and initialized priorityQueue to be empty'''
-		self.name=name
-		self.space=[TimeBlock(0,2400,2400,None)]
-		self.priorityQueue=[]
-		self.maxPriority=None
+		self.name=name #this variable describes the name of the AllocationSpace.
+		self.space=[TimeBlock(0,2400,2400,None)] #this variable is a list of timeBlocks, but is initially has only one primary TimeBlock
+		self.priorityQueue=[] #this variable is a list of task that is kicked throughout the Allocation, it is sorted according to priority
+		self.maxPriority=None #this variable indicates the current highest priority in the current space.
 
 
 
@@ -125,10 +151,10 @@ class AllocationSpace():
 	'''
 	def GetData(self):
 		for i in self.space:
-			startTime=i.startTime-(i.startTime%100)+(i.startTime%100)*(60.0/100)
-			endTime=i.endTime-(i.endTime%100)+(i.endTime%100)*(60.0/100)
-			span=i.span-(i.span%100)+(i.span%100)*(60.0/100)		
-			#i.spa
+			startTime=i.startTime-(i.startTime%100)+(i.startTime%100)*(60.0/100) #this variable is a recomputed startTime for data output purposes
+			endTime=i.endTime-(i.endTime%100)+(i.endTime%100)*(60.0/100) #this variable is a recomputed endTime for data output purposes
+			span=i.span-(i.span%100)+(i.span%100)*(60.0/100) #this variable is a recomputed span for data output purposes	
+
 			print ("\n",int(startTime),"\t",int(endTime),"\t",int(span),end="\t")
 			if isinstance(i.status,FlexibleTask):
 				print (i.status.title,i.status.priority)
@@ -153,7 +179,7 @@ class AllocationSpace():
 		print ("allocating",tflex.title,tflex.duration,tflex.lowerbound,tflex.upperbound)
 
 		#saving current data...
-		oldSpace=[]
+		oldSpace=[] #this cariable is a list the saves the current space in case the Allocation fails and needs to be undone.
 		print (self.space)
 		for i in self.space:
 			j=TimeBlock(i.startTime,i.endTime,i.span,i.status)
@@ -162,7 +188,7 @@ class AllocationSpace():
 			print ("magkapareho")
 
 		#search for possible TimeBlocks.
-		startTB=self.SearchStartingTimeBlock(tflex)
+		startTB=self.SearchStartingTimeBlock(tflex) #this variable keeps track of the current TimeBlock while traversing the Allocation Space
 
 		#search for timeblock that has lower priority.
 		if(startTB==None):
@@ -174,7 +200,7 @@ class AllocationSpace():
 
 		#if startTB is not free, then locate for possible kicks.
 		if(not startTB.isFree()):
-			tasktokick=self.LocateKick(self.space.index(startTB),tflex)
+			tasktokick=self.LocateKick(self.space.index(startTB),tflex) #this variable is a TimeBlocks that needs to be freed/tasks that needs to be kicked
 			if(tasktokick==None):
 				self.space=oldSpace				
 				print ("task cannot be allocated")
@@ -192,7 +218,7 @@ class AllocationSpace():
 				startTB=self.splitAndReturnLeft(startTB,tflex)
 			elif(self.rightSided(startTB,tflex)):
 				startTB=self.splitAndReturnRight(startTB,tflex)			
-		timeRemaining=tflex.duration
+		timeRemaining=tflex.duration #this variable keeps tracks of the remainingTime while doeing the Allocation
 
 
 
@@ -280,8 +306,8 @@ class AllocationSpace():
 					startTB=self.splitAndReturnRight(startTB,tfix)			
 		elif startTB==None:
 			TBtokick=self.LocateKickFix(tfix)
-			tasktokick=[]
-			totalspan=0
+			tasktokick=[] #this variable provides list for the TimeBLocks that needs to be freed and this variable is returned by this method
+			totalspan=0  #this keeps tracks of the current free space generated while Kick method is happening
 
 			for i in TBtokick:
 				totalspan+=i.span
@@ -455,11 +481,11 @@ class AllocationSpace():
 		
 
 		if (isinstance(tflex,FlexibleTask)):
-			newTB1=TimeBlock(startTB.startTime,tflex.lowerbound,tflex.lowerbound-startTB.startTime,None)
+			newTB1=TimeBlock(startTB.startTime,tflex.lowerbound,tflex.lowerbound-startTB.startTime,None) #this variable is the newly generated timeblock because of the split
 		elif isinstance(tflex,FixTask):
-			newTB1=TimeBlock(startTB.startTime,tflex.mustart,tflex.mustart-startTB.startTime,None)
+			newTB1=TimeBlock(startTB.startTime,tflex.mustart,tflex.mustart-startTB.startTime,None) #this variable is the newly generated timeblock because of the split
 
-		newTB2=TimeBlock(newTB1.endTime+tflex.duration,startTB.endTime,startTB.endTime-newTB1.endTime-tflex.duration,None)
+		newTB2=TimeBlock(newTB1.endTime+tflex.duration,startTB.endTime,startTB.endTime-newTB1.endTime-tflex.duration,None) #this variable is the newly generated timeblock because of the split
 		
 		self.space[index].startTime=newTB1.endTime
 		self.space[index].endTime=newTB2.startTime	
@@ -483,7 +509,7 @@ class AllocationSpace():
 	def splitAndReturnLeft(self,startTB,tflex):
 		index=self.space.index(startTB)
 
-		newTB1=TimeBlock(startTB.startTime+tflex.duration,startTB.endTime,startTB.endTime-startTB.startTime-tflex.duration,None)
+		newTB1=TimeBlock(startTB.startTime+tflex.duration,startTB.endTime,startTB.endTime-startTB.startTime-tflex.duration,None) #this variable is the newly generated timeblock because of the split
 		self.space[index].startTime=startTB.startTime
 		self.space[index].endTime=newTB1.startTime
 		self.space[index].span=newTB1.startTime-startTB.startTime
@@ -503,7 +529,7 @@ class AllocationSpace():
 	def splitAndReturnRight(self,startTB,tflex):
 		index=self.space.index(startTB)
 
-		newTB1=TimeBlock(startTB.startTime,startTB.endTime-tflex.duration,startTB.endTime-tflex.duration,None)
+		newTB1=TimeBlock(startTB.startTime,startTB.endTime-tflex.duration,startTB.endTime-tflex.duration,None) #this variable is the newly generated timeblock because of the split
 		self.space[index].startTime=startTB.endTime-tflex.duration
 		self.space[index].endTime=startTB.endTime
 		self.space[index].span=startTB.endTime-startTB.endTime+tflex.duration
@@ -566,7 +592,7 @@ class AllocationSpace():
 			pointer=0
 		else:
 			pointer=counter
-		minPriority=self.space[pointer]
+		minPriority=self.space[pointer] #this keeps tracks of the lowest priority in the PriorityQueue
 
 		for i in self.space:
 			if i.startTime >=tflex.lowerbound and i.startTime<tflex.upperbound:
@@ -589,7 +615,7 @@ class AllocationSpace():
 		Formal parameter is tfix of type FixTask.
 	'''
 	def LocateKickFix(self,tfix):
-		tokick=[]
+		tokick=[] #this variable is a list of tasks to be kicked.
 		for i in self.space:
 			if (isinstance(i.status,FlexibleTask)):
 				'''if(i.startTime<= tfix.mustart and i.endTime>tfix.mustend):
@@ -623,7 +649,7 @@ class AllocationSpace():
 		Formal parameter is tobekicked of type FlexibleTask.
 	'''
 	def Kick(self,tobekicked):
-		heapq.heappush(self.priorityQueue,(tobekicked.priority,tobekicked))
+		heapq.heappush(self.priorityQueue,(tobekicked.priority,tobekicked)) #heapq is a method from one of the modules of python
 		for i in self.space:
 			if isinstance(i.status,FlexibleTask):
 				if i.status.title==tobekicked.title:
@@ -645,12 +671,10 @@ class AllocationSpace():
 		This method doent have any parameters.
 	'''
 	def Merge(self):
-		#def __init__(self,title,duration,mustart,mustend):
-	#	def __init__(self,startTime,endTime,span,status):		
-		#dagdag muna ng fix sa bawat end	
+
 		self.space.insert(0,TimeBlock(0,0,0,(FixTask("temp",0,0,0))))
 		self.space.append(TimeBlock(2400,0,0,(FixTask("temp",0,0,0))))
-		head=tail=0
+		head=tail=0 #this variable is a pointer to the space, this will specify which timeblocks must be merged.
 		while(head<len(self.space)):
 			if self.space[head].isFree():
 				pass
