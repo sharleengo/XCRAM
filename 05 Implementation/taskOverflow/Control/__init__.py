aafba353a4212989c15d98c7a1d782d57b5d9e7a
savanah18 <gpagluba@up.edu.ph>
2017-02-09 09:51:07 +0800

    Update __init__.py

--- 05 Implementation/taskOverflow/Control/__init__.py
+++ 05 Implementation/taskOverflow/Control/__init__.py
@@ -44,7 +44,7 @@ import os
 import math 
 
 import Data.__init__ as Dat
-import UI.__init__ as UI
+#import UI.__init__ as UI
 
 '''
 For all Classes, no files or database tables had been used.
@@ -77,7 +77,7 @@ class AddTask():
 		Formal parameters of this method is task and AllocationSpace of type Task and AllocationSpace respectively
 		AllocationSpace is an instance off the class Allocation in which we will allocate task.
 	'''
-	def addTask(self,task,AllocationSpace):
+	def addTask(self,task,AllocationSpace,Partition=False):
 		'''check parameters of task'''
 
 		if isinstance(task,Dat.FixTask):
@@ -115,7 +115,7 @@ class AddTask():
 
 
 
-			AllocationSpace.AllocateTimeFix(task)	#a method call to AllocateTimeFix
+			AllocationSpace.AllocateTime(task,Partition)	#a method call to AllocateTimeFix
 	
 		elif isinstance(task,Dat.FlexibleTask):
 			if(task.lowerbound%100 > 100 or task.upperbound%100>100 or task.duration%100>100):
@@ -154,45 +154,35 @@ class AddTask():
 				return False
 
 
-			AllocationSpace.AllocateTime(task)#a method call to AllocateTimeFix
+			AllocationSpace.AllocateTime(task,Partition)#a method call to AllocateTimeFix
 
-		AllocationSpace.GetData() 
 		#the code below performs the rescheduling of recently kicked tasks
 		for i in AllocationSpace.priorityQueue:
 			if(AllocationSpace.AllocateTime(i[1])==True):
 				print ("true")
 				AllocationSpace.priorityQueue.pop()
 
+		print ("saving...")
+		AllocationSpace.Save("Data\DataFiles\myData.in")
+
+		AllocationSpace.GetData() 
 
 '''This class is a ControlClass for prompting user inputs.
 Primary attributes is Allocator which an instance of AddTask class.
 Methods is ActiveState
 '''
 class Menu():
-	'''	method  __init__
-		created January 29,2017
 
-		This method initializes an object of class Menu.
-		__init__ methods return None.
-		This method has no parameters.
-	'''
 	def __init__(self):
 		self.Allocator=AddTask() #this is an initialized addTask class
 
 
-	'''method ActiveState
-		created January 29,2017	
 
-		This method is the does the interface between the use and the software itself.
-		It provides prompt which the user have to respond to inorder to perform the Allocation.
-		The method does not return anything.
-		Formal parameters of this method is AllocationSpace of type AllocationSpace.
-	'''	
 	def ActiveState(self,AllocationSpace):
 		while(True):
 			#os.system('cls')
 			AllocationSpace.GetData()		
-			print ("[A].Add task\n")
+			print ("[A].Add task\t[Q].Quit\n")
 			event=raw_input("") #this keeps tracks of the actions specified the user
 			if(event=="A"):
 				newT=None #this is a temporary variablee for the possible task to be created
@@ -207,7 +197,7 @@ class Menu():
 				if(ttype==1):
 					try:
 						mustart=int(raw_input("enter start time\n")) #this variable takes note of the entered mustart
-						mustend=int(raw_input("enter end time\n")) #this variable takes note of the entered mustend
+						mustend=mustart+duration
 					except ValueError as V:
 					    print(V)
 					else:
@@ -217,11 +207,22 @@ class Menu():
 
 				elif(ttype==2):
 					try:
-						priority=int(raw_input("enter priority")) #this variable takes note of the entered priority
+						priority=int(raw_input("enter priority"))#this variable takes note of the entered priority
 						lowerbound=int(raw_input("inter lowerbound\n")) #this variable takes note of the entered lowerbound
 						upperbound=int(raw_input("inter upperbound\n")) #this variable takes note of the entered upperbound
+						Partition=bool(input("do you want to partition?\n[1]Yes\t\t[0]NO\n"))
 					except ValueError as V:
 					    print(V)
 					else:
+						print (Partition)
 						newT=Dat.FlexibleTask(title,duration,priority,lowerbound,upperbound)
-						newT=self.Allocator.addTask(newT,AllocationSpace)
+						newT=self.Allocator.addTask(newT,AllocationSpace,Partition)
+
+
+			if (event=="Q"):
+				break
+		print ("saving...")
+		AllocationSpace.Save("Data\DataFiles\myData.in")
+
+if __name__=="__main__":
+	pass
